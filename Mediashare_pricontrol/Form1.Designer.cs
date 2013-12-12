62ea91ebc906267f769db6296848663a2b5e3a1c
yb19911228 <yb19911228@gmail.com>
2013-12-12 21:26:26 +0800

    ing

---
+++ Mediashare_pricontrol/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Mediashare_pricontrol
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.登录ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关闭ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关闭ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.添加用户ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.用户管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.textBox_pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_id = new System.Windows.Forms.Label();
+            this.label_pass = new System.Windows.Forms.Label();
+            this.button_comfirm = new System.Windows.Forms.Button();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.数据库测试ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.数据库更改ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.登录ToolStripMenuItem,
+            this.设置ToolStripMenuItem,
+            this.添加用户ToolStripMenuItem,
+            this.用户管理ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(453, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 登录ToolStripMenuItem
+            // 
+            this.登录ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关闭ToolStripMenuItem,
+            this.退出ToolStripMenuItem,
+            this.关闭ToolStripMenuItem1});
+            this.登录ToolStripMenuItem.Name = "登录ToolStripMenuItem";
+            this.登录ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.登录ToolStripMenuItem.Text = "开始";
+            // 
+            // 关闭ToolStripMenuItem
+            // 
+            this.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem";
+            this.关闭ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.关闭ToolStripMenuItem.Text = "登录";
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // 关闭ToolStripMenuItem1
+            // 
+            this.关闭ToolStripMenuItem1.Name = "关闭ToolStripMenuItem1";
+            this.关闭ToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.关闭ToolStripMenuItem1.Text = "关闭";
+            this.关闭ToolStripMenuItem1.Click += new System.EventHandler(this.关闭ToolStripMenuItem1_Click);
+            // 
+            // 添加用户ToolStripMenuItem
+            // 
+            this.添加用户ToolStripMenuItem.Name = "添加用户ToolStripMenuItem";
+            this.添加用户ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.添加用户ToolStripMenuItem.Text = "添加用户";
+            // 
+            // 用户管理ToolStripMenuItem
+            // 
+            this.用户管理ToolStripMenuItem.Name = "用户管理ToolStripMenuItem";
+            this.用户管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.用户管理ToolStripMenuItem.Text = "用户管理";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于ToolStripMenuItem,
+            this.关于ToolStripMenuItem1});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.关于ToolStripMenuItem.Text = "使用说明";
+            // 
+            // 关于ToolStripMenuItem1
+            // 
+            this.关于ToolStripMenuItem1.Name = "关于ToolStripMenuItem1";
+            this.关于ToolStripMenuItem1.Size = new System.Drawing.Size(124, 22);
+            this.关于ToolStripMenuItem1.Text = "关于";
+            this.关于ToolStripMenuItem1.Click += new System.EventHandler(this.关于ToolStripMenuItem1_Click);
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(210, 111);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(100, 21);
+            this.textBox_id.TabIndex = 1;
+            // 
+            // textBox_pass
+            // 
+            this.textBox_pass.Location = new System.Drawing.Point(210, 145);
+            this.textBox_pass.Name = "textBox_pass";
+            this.textBox_pass.PasswordChar = '*';
+            this.textBox_pass.Size = new System.Drawing.Size(100, 21);
+            this.textBox_pass.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F);
+            this.label1.Location = new System.Drawing.Point(161, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "管理员登录";
+            // 
+            // label_id
+            // 
+            this.label_id.AutoSize = true;
+            this.label_id.Location = new System.Drawing.Point(143, 111);
+            this.label_id.Name = "label_id";
+            this.label_id.Size = new System.Drawing.Size(29, 12);
+            this.label_id.TabIndex = 4;
+            this.label_id.Text = "帐号";
+            // 
+            // label_pass
+            // 
+            this.label_pass.AutoSize = true;
+            this.label_pass.Location = new System.Drawing.Point(143, 154);
+            this.label_pass.Name = "label_pass";
+            this.label_pass.Size = new System.Drawing.Size(29, 12);
+            this.label_pass.TabIndex = 5;
+            this.label_pass.Text = "密码";
+            // 
+            // button_comfirm
+            // 
+            this.button_comfirm.Location = new System.Drawing.Point(145, 189);
+            this.button_comfirm.Name = "button_comfirm";
+            this.button_comfirm.Size = new System.Drawing.Size(75, 23);
+            this.button_comfirm.TabIndex = 6;
+            this.button_comfirm.Text = "确定";
+            this.button_comfirm.UseVisualStyleBackColor = true;
+            this.button_comfirm.Click += new System.EventHandler(this.button_comfirm_Click);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(235, 189);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(75, 23);
+            this.button_clear.TabIndex = 7;
+            this.button_clear.Text = "清除";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // 设置ToolStripMenuItem
+            // 
+            this.设置ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.数据库测试ToolStripMenuItem1,
+            this.数据库更改ToolStripMenuItem});
+            this.设置ToolStripMenuItem.Name = "设置ToolStripMenuItem";
+            this.设置ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.设置ToolStripMenuItem.Text = "设置";
+            // 
+            // 数据库测试ToolStripMenuItem1
+            // 
+            this.数据库测试ToolStripMenuItem1.Name = "数据库测试ToolStripMenuItem1";
+            this.数据库测试ToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.数据库测试ToolStripMenuItem1.Text = "数据库测试";
+            this.数据库测试ToolStripMenuItem1.Click += new System.EventHandler(this.数据库测试ToolStripMenuItem1_Click);
+            // 
+            // 数据库更改ToolStripMenuItem
+            // 
+            this.数据库更改ToolStripMenuItem.Name = "数据库更改ToolStripMenuItem";
+            this.数据库更改ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.数据库更改ToolStripMenuItem.Text = "数据库更改";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 325);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_comfirm);
+            this.Controls.Add(this.label_pass);
+            this.Controls.Add(this.label_id);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_pass);
+            this.Controls.Add(this.textBox_id);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 登录ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关闭ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关闭ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 添加用户ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 用户管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem1;
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.TextBox textBox_pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_id;
+        private System.Windows.Forms.Label label_pass;
+        private System.Windows.Forms.Button button_comfirm;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.ToolStripMenuItem 设置ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 数据库测试ToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 数据库更改ToolStripMenuItem;
+    }
+}
+
---
+++ Mediashare_pricontrol/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySQLDriverCS;
+
+namespace Mediashare_pricontrol
+{
+    public partial class Form1 : Form
+    {
+        public string DB_Address="10.1.71.175", DB_User="root", DB_Pass="123456", DB_Database="search2", Login="0";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //关闭按钮
+        private void 关闭ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+        //登录参数清空按钮
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            textBox_id.Clear();
+            textBox_pass.Clear();
+        }
+
+        //关于按钮
+        private void 关于ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Zjicm,All Rights Reserved!");
+        }
+
+        //数据库连接测试
+        public Boolean MysqlTest()
+        {
+            Boolean ReturnValue;
+            MySQLConnection conn = null;
+            conn = new MySQLConnection(new MySQLConnectionString(DB_Address, DB_Database, DB_User, DB_Pass).AsString);
+ 
+            try
+            {
+               
+               conn.Open();
+                ReturnValue = true;
+            }
+            catch (Exception e)
+            {
+                //MessageBox.Show("数据库连接失败，请检查参数");
+                ReturnValue = false;
+            }
+            conn.Close();
+            ReturnValue = true;
+            return ReturnValue;
+        }
+
+        //数据库读取
+        public string MysqlRead(string Query)
+        {
+            string ReturnValue = null;
+            MySQLConnection conn = null;
+            MySQLCommand commn = new MySQLCommand("set names utf-8", conn);
+            conn = new MySQLConnection(new MySQLConnectionString(DB_Address, DB_Database, DB_User, DB_Pass).AsString);
+            string query = "select * from video";
+            conn.Open();
+            MySQLCommand cmd = new MySQLCommand(query, conn);
+
+            MySQLDataReader reader = cmd.ExecuteReaderEx();
+            while (reader.Read())
+            {
+                ReturnValue = reader.GetString(0);
+            }
+            conn.Close();
+            return ReturnValue;
+
+        }
+
+        //数据库写入
+        public void MysqlWrite(string Query)
+        {
+            MySQLConnection conn = null;
+            //MySQLCommand commn = new MySQLCommand("set names utf-8", conn);
+            conn = new MySQLConnection(new MySQLConnectionString(DB_Address, DB_Database, DB_User, DB_Pass).AsString);
+            //string query = "select * from video";
+            MySQLCommand cmd = new MySQLCommand(Query, conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();//执行数据库代码
+            conn.Close();
+        }
+
+        private void 数据库测试ToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            if (MysqlTest() == true)
+                MessageBox.Show("数据库连接成功，请登录");
+            else
+                MessageBox.Show("数据库连接失败，请修改参数");
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Login = "0";
+            MessageBox.Show("已推出");
+        }
+
+        private void button_comfirm_Click(object sender, EventArgs e)
+        {
+            string LoginId = textBox_id.Text;
+            string LoginQuery = "SELECT password FROM user WHERE username=" + "'" + LoginId +"'";
+            if (MysqlRead(LoginQuery) == textBox_pass.Text)
+                MessageBox.Show("登录成功");
+            else
+                MessageBox.Show("登录失败，请检查帐户或用户名");
+            MessageBox.Show(LoginQuery);
+        }
+    }
+
+}
